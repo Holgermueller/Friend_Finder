f3f159d0e5a914ffb816845ac50b8e269f3391ba
Holger Mueller <9jholger@gmail.com>
2018-05-09 22:10:16 -0500

    pseudo code for js files

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,4 @@
+//GET route to the url /api/friends for displaying JSON of possible friends
+//POST routes /api/friends for handling incoming survey results
+
+//and compatability logic
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,2 @@
+//GET route to /survey
+//default catch-all route that leads to home.html
