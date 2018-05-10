873ef10f421d022633053a30453722299f1df7b6
Holger Mueller <9jholger@gmail.com>
2018-05-10 11:17:57 -0500

    got home page up through server

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const path = require('path');
 const app = express()
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, "/app/public/home.html")));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
