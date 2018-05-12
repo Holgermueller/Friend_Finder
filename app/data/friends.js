b79dcc4f7012912c098d4ef6b56bb14491554cea
Holger Mueller <9jholger@gmail.com>
2018-05-11 21:04:33 -0500

    formatted friendsjs

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,23 @@
 //store data from survey here as an array of objects.
 //example:
-// {
-//     "name":"Ahmed",
-//     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//     "scores":[
-//         5,
-//         1,
-//         4,
-//         4,
-//         5,
-//         1,
-//         2,
-//         5,
-//         4,
-//         1
-//       ]
-//   }
-let listOfFriends = [{}]
+
+
+let listOfFriends = [{
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}
+]
 
 module.exports = listOfFriends;
