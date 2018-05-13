4027ab5a4c7a190c122718e4fffe30c14969abfb
Holger Mueller <9jholger@gmail.com>
2018-05-13 12:56:41 -0500

    added logic to api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,42 @@
 const listOfFriends = require("../data/friends.js");
-console.log(listOfFriends);
+//console.log(listOfFriends);
 
 //GET route to the url /api/friends for displaying JSON of possible friends
-module.exports = function(app){
-    app.get('/api/friends', function(req, res){
+module.exports = function (app) {
+    app.get('/api/friends', function (req, res) {
         res.json(listOfFriends);
-    })
-}
-//POST routes /api/friends for handling incoming survey results
-// app.post('/api/friends', function(req, res){
-    //if
-//res.json()
-//else
-// })
-//and compatability logic
+    });
+
+    //POST routes /api/friends for handling incoming survey results
+    app.post('/api/friends', function (req, res) {
+        let newFriendsScores = req.body.scores;
+        let arrayForScore = [];
+        let friendCount = 0;
+        let bestMatch = 0;
+
+        //run through current list of friends
+        for (let i = 0; i < listOfFriends.length; i++) {
+            let differenceInScores = 0;
+            //run through scores to compare friends
+            for (let n = 0; n < newFriendsScores.length; n++) {
+                differenceInScores += (Math.abs(parseInt(listOfFriends[i].scores[n] - parseInt(newFriendsScores[n]))));
+            }
+
+            //push results to array
+            arrayForScore.push(differenceInScores);
+        }
+
+        //find best match
+        for (let i = 0; i < arrayForScore.length; i++) {
+            if (arrayForScore[i] <= arrayForScore[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+        //return best match
+        let bestFriendFromArray = listOfFriends[bestMatch];
+        res.json(bestFriendFromArray);
+
+        //push new submisstion into list array
+        listOfFriends.push(req.body);
+    });
+};
