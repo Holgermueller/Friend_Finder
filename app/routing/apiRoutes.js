949b6d72e418add6b6dedc617a613cc69e9569b7
Holger Mueller <9jholger@gmail.com>
2019-01-05 11:14:33 -0600

    updated npm packages

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,6 +12,27 @@ module.exports = (app) => {
         const userInput = req.body;
         //console.log('userInput = ' + JSON.stringify(userInput));
         const userResponse = userInput.score;
-        console.log('userResponse = ' + userResponse);
+        //console.log('userResponse = ' + userResponse);
+        let matchName = '';
+        let totalDiff = 10000;
+
+        //search friends already on list
+        for (let i = 0; i < listOfFriends.length; i++) {
+            //console.log('friend = ' + JSON.stringify(listOfFriends[i]));
+
+            //determine difference
+            let diff = 0;
+            for (let j = 0; j < userResponse.length; j++) {
+                diff += Math.abs(listOfFriends[i].score[j] - userResponse[j]);
+            }
+            if (diff < totalDiff) {
+                totalDiff = diff;
+                matchName = listOfFriends[i].name;
+            }
+        }
+        //add new user
+        listOfFriends.push(userInput);
+        //send apt response
+        res.json({ status: 'OK', matchName });
     });
 };
--- server.js
+++ server.js
@@ -1,24 +1,19 @@
-//requires
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const path = require('path');
 
-//get server running
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-//morgan
 app.use(morgan('tiny'));
 
-//parse data with express
+app.use(express.static(__dirname + "/public"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 
-//ROUTER
 let apiRoute = require('./app/routing/apiRoutes.js')(app);
 let htmlRoute = require('./app/routing/htmlRoutes.js')(app);
 
