14540e818f2f9cd9cf3244797f25575723f6f46d
Holger Mueller <9jholger@gmail.com>
2019-01-05 11:22:41 -0600

    removed unnecessary packages

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const path = require('path');
-
 const app = express()
 const PORT = process.env.PORT || 3000;
 
